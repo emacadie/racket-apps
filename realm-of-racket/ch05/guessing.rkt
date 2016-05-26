3f1e3d24327ca48a3f15103606ee0b556edf7dc8
EMacAdie <EMacAdie@gmail.com>
2016-05-26 00:15:21 -0500

    Worked on the guessing game from chapter 5

---
+++ realm-of-racket/ch05/guessing.rkt
@@ -0,0 +1,63 @@
+#lang racket
+(require 2htdp/universe 2htdp/image)
+(require (prefix-in universe: 2htdp/universe) 
+         (prefix-in image: 2htdp/image))
+(struct interval (small big) #:transparent)
+(define TEXT-SIZE 20)
+(define WIDTH 800)
+(define HEIGHT 400)
+(define SIZE 20)
+(define TEXT-X 3)
+(define TEXT-UPPER-Y 10)
+(define TEXT-LOWER-Y (- HEIGHT 10))
+(define HELP-TEXT
+  (image:text "arrow-up for larger numers, arrow-down for smaller numbers"
+        TEXT-SIZE
+        "blue"))
+(define HELP-TEXT2
+  (image:text "Press = when your number is guessed; q to quit"
+        TEXT-SIZE
+        "blue"))
+(define COLOR "red")
+
+(define MT-SC
+  (image:place-image/align
+   HELP-TEXT TEXT-X TEXT-UPPER-Y "left" "top"
+   (image:place-image/align
+    HELP-TEXT2 TEXT-X TEXT-LOWER-Y "left" "bottom"
+    (image:empty-scene WIDTH HEIGHT))))
+
+(define (deal-with-guess w key)
+  (cond [(universe:key=? key "up") (bigger w)]
+        [(universe:key=? key "down") (smaller w) ]
+        [(universe:key=? key "q") (universe:stop-with w)]
+        [(universe:key=? key "=") (universe:stop-with w)]
+        [else w]))
+
+;; "bigger" can be defined after it is used
+(define (bigger w)
+  (interval (min (interval-big w) (add1 (guess w)))
+            (interval-big w)))
+
+(define (smaller w)
+  (interval (interval-small w)
+            (max (interval-small w) (sub1 (guess w)))))
+
+(define (guess w)
+  (quotient (+ (interval-small w) (interval-big w)) 2))
+
+(define (render w)
+  (image:overlay (text (number->string (guess w)) SIZE COLOR) MT-SC))
+
+(define (render-last-scene w)
+  (image:overlay (text "End" SIZE COLOR) MT-SC))
+
+(define (single? w)
+  (= (interval-small w) (interval-big w)))
+
+;; click "Run" and type "(start 1 100)"
+(define (start lower upper)
+  (universe:big-bang (interval lower upper)
+            (universe:on-key deal-with-guess)
+            (universe:to-draw render)
+            (universe:stop-when single? render-last-scene)))
