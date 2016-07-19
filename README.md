f32140d5fa3d9654e6451f4f734845474fde5bea
EMacAdie <EMacAdie@gmail.com>
2016-07-18 21:59:17 -0500

    Up to section 8.5, kind of done with Racket for the time being

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # racket-apps
 A repo for things I do in Racket
+
+Local Racket docs on my system: file:///home/ericm/racket/doc/index.html
+
--- realm-of-racket/ch08/source.rkt
+++ realm-of-racket/ch08/source.rkt
@@ -1,4 +1,7 @@
 #lang racket
+(require (prefix-in universe: 2htdp/universe) 
+         (prefix-in image: 2htdp/image)) 
+
 
 ;; constants
 (define MAX-HEALTH 35)
@@ -9,12 +12,31 @@
 ;; player's attributes, list of monsters, number of attacks left
 (struct orc-world (player lom attack#) #:transparent)
 ;; struct for player
-(struct player (health agility strength) #:transparent)
+(struct player (health agility strength) #:transparent #:mutable)
+;; creating a player: (define player1 (player 1 2 3))
+;; get the health: (player-health player1)
+;; setting the health: (set-player-health! player1 33)
+;; doing this creates a copy of player1: (define player3 player1)
+;; make a list of players: (define players (list player1 player2 player3))
 
-;; struct for monster
-(struct monster (health) #:transparent)
+;; generic updater for player fields
+(define (player-update! setter selector mx)
+  (lambda (player delta)
+    (setter player (interval+ (selector player) delta mx))))
 
-;; structs for type of monsters
+(define player-health+
+  (player-update! set-player-health! player-health MAX-HEALTH))
+
+(define player-agility+ 
+  (player-update! set-player-agility! player-agility MAX-AGILITY))
+
+(define player-strength+
+  (player-update! set-player-strength! player-strength MAX-STRENGTH))
+
+;; struct for monster, health is mutable, creates "set-monster-health!" mutator
+(struct monster ([health #:mutable]) #:transparent)
+
+;; structs for type of monsters (inheritance)
 ;; you get the health field by calling monster-health: (monster-health my-orc)
 (struct orc monster (club) #:transparent)
 (struct hydra monster () #:transparent)
